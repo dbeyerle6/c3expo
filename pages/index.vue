8141867ab3f1e335068b4abeab2dbc9237afdc84
valkovxyz <valkov.artyom.fs@gmail.com>
2024-03-14 12:29:39 +0200

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-<!--  <div v-if="isUnderConstruction" class="isUnder">-->
-<!--    <h2>Website is under construction</h2>-->
-<!--  </div>-->
+  <div v-if="isUnderConstruction" class="isUnder">
+    <h2>Website is under construction</h2>
+  </div>
   <div class="container" @wheel="handleScroll">
     <Modal :isVisible="modalVisible" :index="selectedIndex" @update:isVisible="closeModal" />
     <div v-if="showIntro" class="intro-container" :key="componentKey">
@@ -46,7 +46,7 @@ const threeContainer = ref(null);
 let camera, scene, renderer, controls, composer;
 const mouse = new THREE.Vector2();
 const raycaster = new THREE.Raycaster();
-const isUnderConstruction = ref(false)
+const isUnderConstruction = ref(true)
 let touchStart = ref(null);
 const selectedCylinderIndexes = [86, 190, 105, 180, 200, 156, 92, 132]; // Пример индексов
 const texts = ['References', 'Unsere Vision', 'Heritage', 'CEO Statement', 'Unsere Mission', 'Unsere Expertiese', 'World wide Network', 'Produkt']; // Пример текстов
