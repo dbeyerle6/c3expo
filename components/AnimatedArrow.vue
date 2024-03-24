6f1350abc1f69dc435dc5f126d8d082b2bdb0ecd
valkovxyz <valkov.artyom.fs@gmail.com>
2024-03-24 19:56:56 +0200

    fix

---
+++ components/AnimatedArrow.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="graph__wrapper">
+    <svg width="315px" height="107px" viewBox="0 0 315 107" version="1.1" style="overflow:visible">
+      <g  id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage">
+        <path id="Path-1" class="path" fill="none" stroke="#36e4da" stroke-width="5" stroke-linejoin="round" stroke-miterlimit="10" d="M1.4,2.1c0,0,86,57,211.5,41.5s172.5-24.5,289,81"/>
+
+        <path class="dashed" fill="none" stroke="white" stroke-width="8" stroke-linejoin="round" stroke-miterlimit="10" d="M1.4,2.1c0,0,86,57,211.5,41.5s172.5-24.5,289,81"/>
+
+        <polyline id="arrow" points="0,-9 18,0 0,9 5,0" fill="#36e4da">
+          <animateMotion rotate="auto" begin="1s" dur="1.6s" repeatCount="1" fill="freeze">
+            <mpath xlink:href="#Path-1" />
+          </animateMotion>
+        </polyline>
+
+      </g>
+    </svg>
+  </div>
+
+</template>
+
+<style scoped>
+.path { stroke-dasharray: 1000; stroke-dashoffset: 1000; animation: dash 3s linear forwards; animation-iteration-count: 1;animation-delay: 1s;}
+@keyframes dash {
+  to {
+    stroke-dashoffset: 0;
+  }
+}
+.dashed{
+  stroke-dasharray: 5,12;
+
+}
+#arrow{animation: arrow 2s linear forwards; opacity:0;}
+@keyframes arrow {
+  to {
+    opacity: 1;
+  }
+}
+</style>
--- i18n.config.ts
+++ i18n.config.ts
@@ -6,31 +6,50 @@ export default defineI18nConfig(() => ({
             ready_for_exp: 'READY for YOUR experience, then click on me!',
             our_mission: {
                 title: 'Our Mission',
-                text1: 'Acting in partnership',
-                text2: 'We always communicate, operate and act as equals in order to meet our customers\' needs before they arise! The decisive factor here is that our actions are characterized by reliability and trust which are deeply embedded in the DNA of our company philosophy.',
-                text3: 'Quality promise',
-                text4: 'Our approach is based on the motto "promise only what you can keep". We keep our promise of quality in all areas of our project business. This creates trust with our customers and forms the basis of our service.',
+                text1: `<strong>Collaborative Action</strong> - We <span style="color: #36e4da">communicate</span>, <span style="color: #36e4da">act</span>, and <span style="color: #36e4da">conduct business</span> always at eye level, in order to serve the <span style="color: #36e4da">needs</span> of our clients even before they arise! Crucial for this is that our actions are characterized by <span style="color: #36e4da">reliability</span> and <span style="color: #36e4da">trust</span>, which are deeply rooted in the <span style="color: #36e4da">DNA</span> of our company philosophy.`,
+                text2: '<strong>Quality Promise</strong> - True to the motto "<span style="color: #36e4da">promise only what you can deliver</span>," is the approach to our actions. Within all areas of our <span style="color: #36e4da">project business</span>, we maintain our <span style="color: #36e4da">quality promise</span>. This creates <span style="color: #36e4da">trust</span> among our customers and forms the basis of our service.',
             },
-            our_vision: {
+                our_vision: {
                 title: 'Our Vision',
+                text: `An <span style="color: #36e4da">encounter</span> should be as <span style="color: #36e4da">sustainable</span> as the space in which it takes place. We aim to evoke <span style="color: #36e4da">emotions</span> in visitors and participants within the <span style="color: #36e4da">brand space</span>, so that an impressive physical <span style="color: #36e4da">encounter</span> can occur.`,
             },
             our_expertise: {
                 title: 'Our Expertise',
+                design: 'Design & Creation',
+                design_text1: 'Right at the beginning, there is this idea floating in our heads. We create a space where this idea can unfold.',
+                design_text2: 'We tell your story and embed it within modern architecture, highlighting your product, thus creating a holistic approach.',
+                ansprechpartner: 'Single Point of Contact',
+                ansprechpartner_text1: 'Our motto is "single point of contact".',
+                ansprechpartner_text2: 'A single point of contact takes care of all your concerns. They will advise you, manage all internal and external tasks, and support you until your project is completed.',
+                messebau: 'Trade Fair Construction',
+                messebau_text: 'Our core area of operation is live marketing with a focus on trade fairs. This is our passion, and we are dedicated to it. Having been active in the market for over 25 years, we can offer our customers everything related to trade fairs.',
+                events: 'Events',
+                events_text: 'An event is only as good as the memories it leaves behind. We create those lasting emotions, thus ensuring your event is a complete success.'
             },
             heritage: {
                 title: 'Heritage',
             },
             product: {
                 title: 'Product',
+                text: 'A trade fair stand is more than just a composition of different components! When entering the stand, the visitor should be taken on a journey in which they are told a story - about the exhibitor, their products and innovations, the values they stand for, but above all that they are personally there for them and discusses his needs and advises him. In this environment of selected materials, colors, staging, lighting and architectural forms, the space of encounter is created. Customers, partners and even friends meet in a place that becomes the focus of visitors and exhibitors for a few days. We create this place, plan it, produce it and build it for you, no matter where in the world!'
             },
             world_wide_network: {
                 title: 'World Wide Network',
+                text: 'We are not personally represented everywhere, but we have a select global network of selected partners who unite us through the same values and dedication to the industry. This enables us to keep our quality promise for our customers worldwide.',
             },
             ceo_statement: {
                 title: 'CEO Statement',
+                text1: `The C³ Expo is a <span style="color: #36e4da">family-owned business</span> that has been active in live marketing with a focus on <span style="color: #36e4da">exhibition construction</span> for more than <span style="color: #36e4da">25 years</span>.
+  `,
+                text2: `The product of our work is accompanied by a consistent <span style="color: #36e4da">quality promise</span> and thereby creates sustainable <span style="color: #36e4da">emotions</span> in the space. Since human <span style="color: #36e4da">encounter</span> is irreplaceable, we create and provide a place for it, which forms the platform for the encounter as such, but also the <span style="color: #36e4da">staging</span> of <span style="color: #36e4da">brands</span> and their <span style="color: #36e4da">story</span>, <span style="color: #36e4da">values</span>, and <span style="color: #36e4da">products</span>.`,
+                text3: `The C³ Expo stands for <span style="color: #36e4da">family values</span> such as <span style="color: #36e4da">trust</span>, <span style="color: #36e4da">reliability</span>, <span style="color: #36e4da">respect</span>, and <span style="color: #36e4da">honesty</span>, which are always highly appreciated by our employees, service providers and partners, as well as our customers.
+    The <span style="color: #36e4da">success story</span> of C³ Expo is set to write many more chapters. For this reason, our <span style="color: #36e4da">value framework</span> is shaped by the partnership idea and determines our actions.
+  `,
             },
             references: {
                 title: 'References',
+                text1: 'BASF SE Milan Expo - The motto was: Can you see the big picture? Through the arrangement of furniture and exhibits, which in their symbiosis from a certain angle reveal a big overall picture. Each individual piece of furniture and exhibit contains a small part of the overall composition. The conceptual approach came from DING3000 and was brought to life with our cooperation in the historic old town of Milan.',
+                text2: 'BASF SE Düsseldorf Plastics Fair – The Mecca for the international plastics industry. Since the beginning of the 2000s, C³ Expo GmbH has been supporting BASF every four years with the development of concepts for the staging of its exhibits and products and has looked after them from the beginning, through the execution on site, right through to the completion of the project. As a home and yard service provider, C³ Expo supports BASF with all of its expertise in a partnership that has now lasted over 20 years with the heavyweight of the German and international chemical industry.',
             },
             links: {
                 privacy_name: 'Privacy Policy',
@@ -52,32 +71,53 @@ export default defineI18nConfig(() => ({
         de: {
             ready_for_exp: 'BEREIT für DEIN Erlebnis, dann klick mich an!',
             our_mission: {
-                title: 'Unsere Mission',
-                text1: 'Partnerschaftliches Handeln',
-                text2: ' Wir kommunizieren, agieren und handeln immer auf Augenhöhe, um die Bedürfnisse unserer Kunden bedienen zu können, bevor diese erst entstehen! Entscheidend hierfür ist, dass unser Handeln geprägt ist von Zuverlässigkeit und Vertrauen, die in der DNA unserer Firmenphilosophie tief verankert ist.',
-                text3: 'Qualitätsversprechen',
-                text4: ' Ganz nach dem Motto „promise only what you can keep“ ist die Herangehensweise unseres Handelns. Innerhalb aller Bereiche unseres Projektgeschäfts, halten wir unser Qualitätsversprechen. Das schafft Vertrauen bei unseren Kunden und bildet die Basis unserer Dienstleistung.',
+                title: 'Our Mission',
+                text1: `<strong>Partnerschaftliches Handeln</strong> - Wir <span style="color: #36e4da">kommunizieren</span>. <span style="color: #36e4da">agieren</span> und <span style="color: #36e4da">handeln</span> immer auf Augenhöhe, um die <span style="color: #36e4da">Bedürfnisse</span> unserer Kunden bedienen, zu können. bevor diese erst entstehen! Entscheidend, hierfür ist, dass unser Handeln geprägt, ist, von <span style="color: #36e4da">Zuverlässigkeit</span> und <span style="color: #36e4da">Vertrauen</span>, die in der <span style="color: #36e4da">DNA</span> unserer Firmenphilosophie tief verankert ist.`,
+                text2: `<strong>Qualitätsversprechen</strong> - Ganz nach dem Motto „<span style="color: #36e4da">promise only what you can keep</span>" ist die Herangehensweise, unseres Handelns. Innerhalb aller Bereiche. unseres. <span style="color: #36e4da">Proiektaeschäfts</span>, halten wir unser <span style="color: #36e4da">Qualitätsversprechen</span>. Das schafft <span style="color: #36e4da">Vertrauen</span> bei unseren Kunden und bildet die Basis unserer Dienstleistung.`
             },
             our_vision: {
                 title: 'Unsere Vision',
+                text: `Eine <span style="color: #36e4da">Begegnung</span> sollte genauso <span style="color: #36e4da">nachhaltig</span> sein, wie der Raum, in dem diese stattfindet. Wir möchten <span style="color: #36e4da">Emotionen</span> bei den Besuchen und Teilnehmen im <span style="color: #36e4da">Markenraum</span> wecken, damit eine beeindruckende physische <span style="color: #36e4da">Begegnung</span> stattfinden kann.</p>
+        `,
             },
             our_expertise: {
-                title: 'Unsere Expertise',
+                title: 'Our Expertise',
+                design: 'Design & Kreation',
+                design_text1: ' Ganz am Anfang, da ist diese Idee, die in unseren Köpfen schwebt. Wir kreieren einen Raum, in dem diese Idee sich entfalten kann.',
+                design_text2: ' Wir erzählen Ihre Geschichte und betten sie zwischen moderner Architektur ein und heben Ihr Produkt hervor, so dass ein ganzheitlicher Ansatz geschaffen wird.',
+                ansprechpartner: 'Ein Ansprechpartner',
+                ansprechpartner_text1: 'Unsere Devise lautet „single point of contact“.',
+                ansprechpartner_text2: 'Ein Ansprechpartner kümmert sich um all Ihre Belange. Er berät Sie, steuert alle internen und externen Aufgaben und betreut Sie, bis Ihr Projekt abgeschlossen ist.',
+                messebau: 'Messebau',
+                messebau_text: 'Unser Kernhandlungsfeld ist das Live Marketing mit Schwerpunkt Messen. Hier liegt unsere Passion und hierfür brennen wir. Seit über 25 Jahren im Markt tätig können wir unseren Kunden alles rund um Messen anbieten.',
+                events: 'Events',
+                events_text: 'Ein Event ist nur so gut, wie das Erlebnis, das ich daraus mitnehme. Diese nachhaltigen Emotionen schaffen wir und machen somit auch Ihr Event zum vollen Erfolg.'
             },
             heritage: {
                 title: 'Heritage',
             },
             product: {
                 title: 'Product',
+                text: 'Ein Messestand ist mehr als eine reine Komposition von verschiedenen Bauteilen! Der Besucher soll beim Betreten des Standes auf eine Reise mitgenommen werden, bei dem ihm eine Geschichte erzählt wird - über den Aussteller, seine Produkte und Innovationen, die Werte, für die er steht, aber vor allem, dass er persönlich für ihn da ist und seine Bedürfnisse bespricht und ihn berät. In diesem Umfeld von ausgewählten Materialien, Farben, Inszenierungen, Beleuchtungen und architektonischen Formen, entsteht der Raum der Begegnung. Kunden, Partner und sogar Freunde, treffen sich an einem Ort, der für ein paar Tage den Mittelpunkt von Besuchern und Ausstellern bildet. Diesen Ort, kreieren wir, planen wir, produzieren ihn und bauen ihn, egal wo auf der Welt, für Sie auf!',
             },
             world_wide_network: {
                 title: 'World Wide Network',
+                text: 'Wir sind nicht überall persönlich vertreten, aber wir verfügen über ein ausgewähltes globales Netzwerk an ausgewählten Partner, die uns durch die gleichen Werte und die Hingabe zur Branche verbinden. So sind wir in der Lage, für unsere Kunden, stets unser Qualitätsversprechen weltweit einhalten zu können.',
             },
             ceo_statement: {
                 title: 'CEO Statement',
+                text1: `Die C³ Expo ist ein <span style="color: #36e4da">Familienunternehmen</span>, das bereits seit mehr als <span style="color: #36e4da">25 Jahren</span> im Livemarketing mit Schwerpunkt <span style="color: #36e4da">Messebau</span> tätig ist.
+          `,
+                text2: `Das Produkt unserer Arbeit wird begleitet durch ein konsistentes <span style="color: #36e4da">Qualitätsversprechen</span> und schafft dadurch nachhaltige <span style="color: #36e4da">Emotionen</span> im Raum. Da die menschliche <span style="color: #36e4da">Begegnung</span> ersatzlos ist, kreieren und schaffen wir einen Ort dafür, der die Plattform für die Begegnung als solches bildet, aber auch die <span style="color: #36e4da">Inszenierung</span> von <span style="color: #36e4da">Marken</span> und deren <span style="color: #36e4da">Geschichte</span>, <span style="color: #36e4da">Werte</span> und <span style="color: #36e4da">Produkte</span> bietet.
+          `,
+                text3: `Die C³ Expo steht für <span style="color: #36e4da">Familienwerte</span> wie <span style="color: #36e4da">Vertrauen</span>, <span style="color: #36e4da">Verlässlichkeit</span>, <span style="color: #36e4da">Respekt</span> und <span style="color: #36e4da">Ehrlichkeit</span>, die stets von unseren Mitarbeitern, Dienstleistern und Partnern, sowie unseren Kunden sehr geschätzt sind.
+            Die <span style="color: #36e4da">Erfolgsgeschichte</span> der C³ Expo soll noch viele weitere Kapitel schreiben. Aus diesem Grund ist unser <span style="color: #36e4da">Wertegerüst</span> durch den partnerschaftlichen Gedanken geprägt und bestimmt unser Handeln.
+         `,
             },
             references: {
                 title: 'References',
+                text1: 'BASF SE Mailand Expo - Das Motto war: Siehst du das Big Picture? Durch die Anordnung von Möbeln und Exponaten, die in ihrer Symbiose von einem bestimmten Blickwinkel ein großes Gesamtbild erkennen lässt. Jedes einzelne Möbelstück und Exponat beinhaltet einen kleinen Teil zur Gesamtkomposition. Konzeptioneller Ansatz kam DING3000 und wurde mit unserer Kooperation in der historischen Altstadt von Mailand zum Leben erweckt.',
+                text2: 'BASF SE Düsseldorf Kunststoffmesse – Das Mekka für die internationale Kunststoffindustrie. Seit Beginn der 2000er, unterstützt die C³ Expo GmbH konstant alle vier Jahre die BASF bei der Konzepterarbeitung in der Inszenierung ihrer Exponate und Produkte und betreut diese von Beginn an, über die Ausführung vor Ort, bis hin zur Abwicklung des Projekts. Als Haus und Hof-Dienstleister steht die C³ Expo der BASF mit all ihrer Expertise in einer mittlerweile über 20 jährigen Partnerschaft, mit dem Schwergewicht der deutschen und internationalen Chemieindustrie, zur Seite.',
             },
             links: {
                 privacy_name: 'Datenschutzrichtlinie',
