de3f1d3d468a92126287a91340c731799068b515
valkovxyz <valkov.artyom.fs@gmail.com>
2024-05-24 01:04:52 +0300

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -785,7 +785,7 @@ body {
   }
 }
 
-@media only screen and (min-device-width: 768px) and (max-device-width: 1366px) and (orientation: portrait){
+@media only screen and (min-device-width: 768px) and (max-device-width: 1366px) and (orientation: landscape){
   body {
     background-size: 500%;
     background-position: center -800px;
